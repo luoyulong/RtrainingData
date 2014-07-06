9dff57706e2147bae42168539bac004e926e8cc8
luoyulong <luoceanskold@qq.com>
2014-07-06 20:34:51 +0800

    edit paint

--- paint.R
+++ paint.R
@@ -18,7 +18,7 @@ GetRGB <- function(col)
   return (r)
 }
 
-GetVariantInfo <- function(sid,number=-1,opttype=""){
+GetVariantInfo <- function(sid,opttype="",number=-1){
   conn <- odbcConnect("myhps","hps","hps")
   if(opttype=="")
     selcmd <- sprintf('select Gflops, OptConfig from optVariant where SpecificsId=%d order by Gflops desc;',sid)
@@ -27,12 +27,35 @@ GetVariantInfo <- function(sid,number=-1,opttype=""){
   sqlQuery(conn,"use hps")
   result <- sqlQuery(conn,selcmd,stringsAsFactors = FALSE)
   close(conn)
- # print(nrow(result))
+  # print(nrow(result))
   if(number==-1)
     return (result)
   else 
     return (result[1:number,])
 }
+
+GetVariantInfo_noOrder <- function(sid,opttype="",number=-1){
+  conn <- odbcConnect("myhps","hps","hps")
+  if(opttype=="")
+    selcmd <- sprintf('select Gflops, OptConfig from optVariant where SpecificsId=%d;',sid)
+  else
+    selcmd <- sprintf('select Gflops, OptConfig from optVariant where SpecificsId=%d and OptType="%s" ;',sid,opttype)
+  sqlQuery(conn,"use hps")
+  result <- sqlQuery(conn,selcmd,stringsAsFactors = FALSE)
+  close(conn)
+  # print(nrow(result))
+  if(number==-1)
+    return (result)
+  else 
+    return (result[1:number,])
+}
+
+
+
+
+
+
+
 
 GetSimilarNumber <- function(A,B)
 {
@@ -46,21 +69,89 @@ GetSimilarNumber <- function(A,B)
 GetSimilarFactor <- function(a_id,b_id,size)
 {
   
-  result1  <- GetVariantInfo(a_id,size,"Tiling")
-  result2 <- GetVariantInfo(b_id,size,"Tiling")
+  result1  <- GetVariantInfo(a_id,"Tiling",size)
+  result2 <- GetVariantInfo(b_id,"Tiling",size)
   similarnumber <- GetSimilarNumber(result1$OptConfig,result2$OptConfig)
   print(sprintf("Similar Rate: %d/%d",similarnumber,nrow(result1)))
 }
-basesize <- 10
+
+
+CompareTuning <- function(id_target,id_predict,opttype,per)
+{
+variants <- GetVariantInfo_noOrder(id_target,opttype)
+variants2 <- GetVariantInfo(id_predict,opttype)
+for(i in 1:nrow(variants2))
+{
+  prdconfig <- variants2[i,]$OptConfig
+  prdGflops <- variants[variants$OptConfig==prdconfig,]$Gflops
+  if(length(prdGflops)>0) 
+    variants2[i,]$Gflops <- prdGflops
+  else
+    variants2[i,]$Gflops <- 0.00001
+}
+
+tuningprocess <- data.frame(step=1,max=variants[1,]$Gflops)
+for(i in 2:nrow(variants))
+{
+  if(variants[i,]$Gflops > tuningprocess[i-1,]$max)
+    submax <- variants[i,]$Gflops
+  else
+    submax <- tuningprocess[i-1,]$max
+  tuningprocess[i,]$step <- i
+  tuningprocess[i,]$max <- submax
+}
+
+prdprocess <- data.frame(step=1,max=variants2[1,]$Gflops)
+for(i in 2:nrow(variants2))
+{
+  if(variants2[i,]$Gflops > prdprocess[i-1,]$max)
+    submax <- variants2[i,]$Gflops
+  else
+    submax <- prdprocess[i-1,]$max
+  prdprocess[i,]$step <- i
+  prdprocess[i,]$max <- submax
+}
+#prdprocess <- rbind(data.frame(step=0,max=min(tuningprocess$max)),prdprocess)
+
+plot(tuningprocess$step,tuningprocess$max,
+     ylim=c(min(tuningprocess$max),max(tuningprocess$max)),
+     xlim=c(1,max(tuningprocess$step)),
+     pch=19,type="l",xlab="tuning steps",ylab="best performance (Gflops)")
+lines(prdprocess$step,prdprocess$max,col="red")
+
+maxperf <- max(prdprocess$max)
+for(i in 1:nrow(prdprocess))
+{
+  
+  if(prdprocess[i,]$max >= maxperf*per)
+  {
+    print(sprintf("achieve in %d,%f \n",i,prdprocess[i,]$max))
+    lines(i,prdprocess[i,]$max, type="h", col=2, lwd=2, lty=2);
+    break;
+  }
+}
+
+}
+
+CompareTuning(12698,12703,"CUDABlocking",0.99)
+ 
+
+
+
+
+
+
 #GetSimilarFactor(12699,12682,basesize)
 #GetSimilarFactor(12699,12683,basesize)
 #GetSimilarFactor(12699,12686,basesize)
 #GetSimilarFactor(12699,12687,basesize)
 
+
+basesize <- 30
 GetSimilarFactor(12699,12701,basesize)
-GetSimilarFactor(12699,12704,basesize)
-GetSimilarFactor(12699,12705,basesize)
-GetSimilarFactor(12699,12708,basesize)
+#GetSimilarFactor(12699,12704,basesize)
+#GetSimilarFactor(12699,12705,basesize)
+#GetSimilarFactor(12699,12708,basesize)
 
 
 #result <- GetVariantInfo(12703)
